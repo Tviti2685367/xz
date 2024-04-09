d49a388538de7899f07bba805bc2557b5f010a26
Титаренко Олександр <145334616+Tviti2685367@users.noreply.github.com>
2024-04-09 17:17:42 +0300

    Add files via upload

---
+++ src/Pr2/HTMLTableResultRenderer.java
@@ -0,0 +1,7 @@
+public class HTMLTableResultRenderer implements CalculationResultRenderer {
+    @Override
+    public String render(CalculationResult result) {
+        // Implementation for rendering as HTML table
+        return "<html><body>HTML table representation of the calculation result</body></html>";
+    }
+}
